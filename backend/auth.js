1bdbb1d1e45ce96b66811293edc62cb243d0d11f
anishapant21 <anishapant1@gmail.com>
2024-11-01 17:49:33 -0400

    Move to env file and create reusable function

--- backend/auth.js
+++ backend/auth.js
@@ -1,47 +1,31 @@
+// main.js (or index.js / app.js)
+const { ldapConfig, mysqlConfig } = require("./config");
 const { authenticate } = require("ldap-authentication");
 const { getPool } = require("./db");
-const readline = require("readline");
+const { askQuestion } = require("./utils/utils");
 
-// Function to prompt for user input
-function askQuestion(query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  return new Promise((resolve) =>
-    rl.question(query, (answer) => {
-      rl.close();
-      resolve(answer);
-    })
-  );
-}
-
-// Function to authenticate user and fetch additional info from MySQL
 async function authenticateAndFetchUser(username, password) {
   const options = {
     ldapOpts: {
-      url: "ldap://localhost:1389",
+      url: ldapConfig.url,
     },
-    userDn: `cn=${username},ou=users,dc=mieweb,dc=com`,
+    userDn: `cn=${username},ou=users,${ldapConfig.baseDN}`,
     userPassword: password,
-    userSearchBase: "dc=mieweb,dc=com",
+    userSearchBase: ldapConfig.userSearchBase,
     usernameAttribute: "cn",
     username: username,
     attributes: ["cn"],
   };
 
   try {
-    // Authenticate the user against LDAP
     let user = await authenticate(options);
     console.log("User Authentication successful:", user);
 
-    // Fetch additional info from MySQL
     const pool = await getPool();
     const connection = await pool.getConnection();
 
-    // Query to fetch additional data using 'cn'
     const [rows] = await connection.execute(
-      "SELECT * FROM user_details WHERE user_name = ?",
+      `SELECT * FROM ${mysqlConfig.tableName} WHERE user_name = ?`,
       [user.cn]
     );
 
@@ -57,11 +41,9 @@ async function authenticateAndFetchUser(username, password) {
   }
 }
 
-// Main function to get user input and execute the authentication
 async function main() {
   const username = await askQuestion("Enter Username: ");
   const password = await askQuestion("Enter Password: ");
-
   await authenticateAndFetchUser(username, password);
 }
 
