d9c2eb7dffbaad3a2ee47635eeb41a398a2ce808
anishapant21 <anishapant1@gmail.com>
2024-12-02 16:36:25 -0500

    Use salt password while storing the user in db

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -14,65 +14,8 @@ CREATE TABLE users (
   home_directory VARCHAR(200)
 );
 
--- Function to generate a random salt
-DELIMITER //
-CREATE FUNCTION generate_salt() 
-RETURNS VARCHAR(50)
-BEGIN
-  RETURN SUBSTRING(MD5(RAND()), 1, 50);
-END //
-DELIMITER ;
-
--- Stored procedure to insert a user with hashed password
-DELIMITER //
-CREATE PROCEDURE insert_user(
-  IN p_username VARCHAR(50),
-  IN p_password VARCHAR(50),
-  IN p_full_name VARCHAR(100),
-  IN p_email VARCHAR(100),
-  IN p_uid_number INT,
-  IN p_gid_number INT,
-  IN p_home_directory VARCHAR(200)
-)
-BEGIN
-  DECLARE v_salt VARCHAR(50);
-  DECLARE v_hashed_password VARCHAR(128);
-  
-  -- Generate salt
-  SET v_salt = generate_salt();
-  
-  -- Hash password with salt using SHA-512
-  SET v_hashed_password = CONCAT(
-    HEX(
-      UNHEX(
-        SHA2(
-          CONCAT(p_password, v_salt), 
-          512
-        )
-      )
-    )
-  );
-  
-  -- Insert user
-  INSERT INTO users (
-    username, password, salt, full_name, 
-    email, uid_number, gid_number, home_directory
-  ) VALUES (
-    p_username, v_hashed_password, v_salt, p_full_name,
-    p_email, p_uid_number, p_gid_number, p_home_directory
-  );
-END //
-DELIMITER ;
-
--- Insert example users
-CALL insert_user(
-  'mie', 'mie', 'MIE User', 
-  'mie@mieweb.com', 1001, 1001, 
-  '/home/mie'
-);
-
-CALL insert_user(
-  'admin', 'adminpass', 'System Administrator', 
-  'admin@mieweb.com', 1000, 0, 
-  '/home/admin'
-);
+-- Insert example users (manual hash for simplicity in init script)
+INSERT INTO users (username, password, salt, full_name, email, uid_number, gid_number, home_directory)
+VALUES
+  ('ann', '21c4474515c9869005f9de3f75c083eaf092bd9f8d5461c7c617f88a3fa32253e2abfbeda31a80b34fa38e374e4602d8b04db55f6e52e84c4bcf59fe9a585eb1', '09c3b732633eb3e92fd05b4dadf50254', 'Ann', 'ann@mieweb.com', 1001, 1001, '/home/ann'),
+  ('admin', '21c4474515c9869005f9de3f75c083eaf092bd9f8d5461c7c617f88a3fa32253e2abfbeda31a80b34fa38e374e4602d8b04db55f6e52e84c4bcf59fe9a585eb1', '09c3b732633eb3e92fd05b4dadf50254', 'System Administrator', 'admin@mieweb.com', 1000, 0, '/home/admin');
