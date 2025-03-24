e448d7f311859f896ed7ebd44fb8e40a8c5f48e4
anishapant21 <anishapant1@gmail.com>
2025-03-24 15:00:39 -0400

    Move ldap conf to env

---
+++ docker/client/ldap_env.sh
@@ -0,0 +1,8 @@
+# LDAP Environment Configuration
+export LDAP_BASE="dc=mieweb,dc=com"
+export LDAP_DOMAIN="mieweb.com"
+export LDAP_ORG="MIE"
+export SSH_PORT=2222
+export LDAP_CERT_SUBJ="/C=US/ST=IN/L=City/O=MIE/CN=localhost"
+export LDAP_URI="ldaps://54.159.200.186:636"
+export LDAP_CERT="/certificates/ca-cert.pem"
