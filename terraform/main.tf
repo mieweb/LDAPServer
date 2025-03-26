efd60b1a464bc6586a3498ed5d67ce08c29048f4
anishapant21 <anishapant1@gmail.com>
2025-03-26 18:16:04 -0400

    Update inbound rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "ldap_sg" {
-  name        = "ldap-security-group"
+  name        = "ldap-security-group-new"  # Changed name to be unique
   description = "Allow LDAP, API, and SSH (for deployments)"
 
   ingress {
@@ -50,4 +50,10 @@ resource "aws_instance" "ldap_server" {
               npm install
               nohup node server.js > server.log 2>&1 &
               EOF
+}
+
+# Add output to display the public IP address
+output "ldap_server_public_ip" {
+  value = aws_instance.ldap_server.public_ip
+  description = "The public IP address of the LDAP server"
 }
